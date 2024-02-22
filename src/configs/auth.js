4e3ad32a9a1473756e75d8c835f5fb3111515ab8
RichardPickman <drezzerock@gmail.com>
2024-02-22 19:26:08 +0100

    fix unnamed import issue and add api url to auth config (temp)

--- src/configs/auth.js
+++ src/configs/auth.js
@@ -1,7 +1,13 @@
-export default {
-  meEndpoint: '/auth/me',
-  loginEndpoint: '/jwt/login',
-  registerEndpoint: '/jwt/register',
-  storageTokenKeyName: 'accessToken',
-  onTokenExpiration: 'refreshToken' // logout | refreshToken
+
+const API_URL = 'https://stg-api.accelerate.unic.ac.cy';
+
+const endpoints = {
+  meEndpoint: API_URL + '/auth/me',
+  loginEndpoint: API_URL + '/auth/login',
+  registerEndpoint: API_URL + '/auth/signUp',
+  forgotMyPasswordEndpoint: API_URL + '/verify/forgotMyPassword',
+  storageTokenKeyName: API_URL + '/accessToken',
+  onTokenExpiration: API_URL + '/refreshToken' // logout | refreshToken
 }
+
+export default endpoints;
