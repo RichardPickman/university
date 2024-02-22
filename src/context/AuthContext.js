065093e8cba3f8b9d2a6381662afe8d16c684ecc
RichardPickman <drezzerock@gmail.com>
2024-02-22 19:23:02 +0100

    revisit whole auth context due to migration from context to zustand

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,103 +1,47 @@
 'use client';
 
-// ** React Imports
-import { createContext, useEffect, useState } from 'react';
-
-// ** Next Import
-import { useRouter } from 'next/navigation';
-
-// ** Axios
 import axios from 'axios';
-
-// ** Config
 import authConfig from 'src/configs/auth';
+import { create } from 'zustand';
 
-// ** Defaults
-const defaultProvider = {
+export const useUserStore = () => create((set) => ({
   user: null,
-  loading: true,
-  setUser: () => null,
-  setLoading: () => Boolean,
-  login: () => Promise.resolve(),
-  logout: () => Promise.resolve()
-}
-const AuthContext = createContext(defaultProvider)
-
-const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(defaultProvider.user)
-  const [loading, setLoading] = useState(defaultProvider.loading)
-
-  const router = useRouter()
-  useEffect(() => {
-    const initAuth = async () => {
-      const storedToken = window.localStorage.getItem(authConfig.storageTokenKeyName)
-      if (storedToken) {
-        setLoading(true)
-        await axios
-          .get(authConfig.meEndpoint, {
-            headers: {
-              Authorization: storedToken
-            }
-          })
-          .then(async response => {
-            setLoading(false)
-            setUser({ ...response.data.userData })
-          })
-          .catch(() => {
-            localStorage.removeItem('userData')
-            localStorage.removeItem('refreshToken')
-            localStorage.removeItem('accessToken')
-            setUser(null)
-            setLoading(false)
-            if (authConfig.onTokenExpiration === 'logout' && !router.pathname.includes('login')) {
-              router.push('/login')
-            }
-          })
-      } else {
-        setLoading(false)
-      }
-    }
-    initAuth()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
-  const handleLogin = (params, errorCallback) => {
+  loading: false,
+  setUser: (data) => set((state) => ({ ...state, user: data })),
+  login: (data) => new Promise((resolve, reject) => {
     axios
-      .post(authConfig.loginEndpoint, params)
-      .then(async response => {
-        params.rememberMe
-          ? window.localStorage.setItem(authConfig.storageTokenKeyName, response.data.accessToken)
-          : null
-        const returnUrl = router.query.returnUrl
-        setUser({ ...response.data.userData })
-        params.rememberMe ? window.localStorage.setItem('userData', JSON.stringify(response.data.userData)) : null
-        const redirectURL = returnUrl && returnUrl !== '/' ? returnUrl : '/'
+      .post(authConfig.loginEndpoint, data)
+      .then(response => {
+        set({ user: response.data.userData })
 
-        router.push(redirectURL)
-      })
-      .catch(err => {
-        if (errorCallback) errorCallback(err)
-      })
-  }
+        if (data.rememberMe) {
+          window.localStorage.setItem('userData', JSON.stringify(response.data.userData))
+          window.localStorage.setItem(authConfig.storageTokenKeyName, response.data.accessToken)
+        }
 
-  const handleLogout = () => {
-    setUser(null)
+        resolve();
+      })
+      .catch(reject)
+  }),
+  register: (data) => new Promise((resolve, reject) => {
+    axios
+      .post(authConfig.registerEndpoint, data)
+      .then(resolve)
+      .catch(reject)
+  }),
+  logout: () => {
+    set((state) => ({ ...state, user: null }))
+
     window.localStorage.removeItem('userData')
     window.localStorage.removeItem(authConfig.storageTokenKeyName)
-    router.push('/login')
-  }
+  },
+  forgotMyPassword: (data) => new Promise((resolve, reject) => {
+    const token = window.localStorage.getItem(authConfig.storageTokenKeyName);
 
-  const values = {
-    user,
-    loading,
-    setUser,
-    setLoading,
-    login: handleLogin,
-    logout: handleLogout
-  }
-
-  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>
-}
-
-export { AuthContext, AuthProvider };
+    axios
+      .post(authConfig.forgotMyPasswordEndpoint, { ...data, token })
+      .then(resolve)
+      .catch(reject)
+  })
+}));
 
