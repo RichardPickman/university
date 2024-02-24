e01eb2a88b4eb29005aba8abaddec624ce699bfa
RichardPickman <drezzerock@gmail.com>
2024-02-24 22:15:00 +0100

    add useStaticImages hook

---
+++ src/hooks/useStaticImages.jsx
@@ -0,0 +1,35 @@
+import forgotPasswordIllustrationDark from "public/images/pages/auth-v2-forgot-password-illustration-dark.png";
+import forgotPasswordIllustrationLight from "public/images/pages/auth-v2-forgot-password-illustration-light.png";
+import LoginIllustrationBorderedDark from "public/images/pages/auth-v2-login-illustration-bordered-dark.png";
+import LoginIllustrationBorderedlight from "public/images/pages/auth-v2-login-illustration-bordered-light.png";
+import LoginIllustrationDark from "public/images/pages/auth-v2-login-illustration-dark.png";
+import LoginIllustrationLight from "public/images/pages/auth-v2-login-illustration-light.png";
+import registerIllustrationBorderedDark from "public/images/pages/auth-v2-register-illustration-bordered-dark.png";
+import registerIllustrationBorderedLight from "public/images/pages/auth-v2-register-illustration-bordered-light.png";
+import registerIllustrationDark from "public/images/pages/auth-v2-register-illustration-dark.png";
+import registerIllustrationLight from "public/images/pages/auth-v2-register-illustration-light.png";
+import miscMaskDark from "public/images/pages/misc-mask-dark.png";
+import miscMaskLight from "public/images/pages/misc-mask-light.png";
+
+export const useStaticImages = (path) => {
+    console.log(path);
+    const paths = {
+        "forgot-password-illustration-dark": forgotPasswordIllustrationDark,
+        "forgot-password-illustration-light": forgotPasswordIllustrationLight,
+        "login-illustration-bordered-dark": LoginIllustrationBorderedDark,
+        "login-illustration-bordered-light": LoginIllustrationBorderedlight,
+        "login-illustration-dark": LoginIllustrationDark,
+        "login-illustration-light": LoginIllustrationLight,
+        "register-illustration-bordered-dark": registerIllustrationBorderedDark,
+        "register-illustration-bordered-light":
+            registerIllustrationBorderedLight,
+        "register-illustration-dark": registerIllustrationDark,
+        "register-illustration-light": registerIllustrationLight,
+        "misc-mask-dark": miscMaskDark,
+        "misc-mask-light": miscMaskLight,
+    };
+
+    return {
+        src: paths[path],
+    };
+};
