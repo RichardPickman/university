d0dc70f5499e0a6ea8d0e412c027cebbd7a613ca
RichardPickman <drezzerock@gmail.com>
2024-02-25 22:39:10 +0100

    add mocked data to "match" the UI example

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,9 +1,13 @@
 "use client";
 
 import AppsIcon from "@mui/icons-material/Apps";
+import BubbleChartIcon from "@mui/icons-material/BubbleChart";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import FolderIcon from "@mui/icons-material/Folder";
 import HomeIcon from "@mui/icons-material/Home";
+import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
 import Shop2Icon from "@mui/icons-material/Shop2";
-import { Button, Divider, IconButton } from "@mui/material";
+import { Button, Divider, IconButton, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -13,9 +17,12 @@ import { useUserStore } from "src/store/userStore";
 const Page = () => {
     const [isOpen, setOpen] = useState(true);
     const logout = useUserStore().getState().logout;
-    const headerHeight = "4rem";
+    const headerHeight = 6;
+    const sidebarWidth = 14;
     const router = useRouter();
+    const mainContentWidth = (isOpen ? sidebarWidth : 0) + 6 + "rem";
 
+    // Using the zustand for this project was a huge mistake.... Ask me later :)
     const onLogoutClick = () => {
         logout();
 
@@ -29,7 +36,7 @@ const Page = () => {
                     display: "flex",
                     justifyContent: "space-between",
                     alignItems: "center",
-                    height: headerHeight,
+                    height: headerHeight + "rem",
                 }}
             >
                 <Box>UNIC</Box>
@@ -48,20 +55,24 @@ const Page = () => {
                     <Button onClick={onLogoutClick}>Logout</Button>
                 </Box>
             </Box>
-            <Sidebar
-                show={isOpen}
-                sx={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    gap: 2,
-                    flexDirection: "column",
-                }}
-                direction="left"
-                onOpen={() => setOpen(true)}
-                onClose={() => setOpen(false)}
-                backDropClick={() => setOpen(false)}
+            <Box
+                id="menu-box"
+                sx={{ display: "flex", height: "100%", position: "relative" }}
             >
-                <Box sx={{ padding: "1rem", height: "100%" }}>
+                <Sidebar
+                    show={isOpen}
+                    sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        gap: 2,
+                        height: "100%",
+                        flexDirection: "column",
+                    }}
+                    direction="left"
+                    onOpen={() => setOpen(true)}
+                    onClose={() => setOpen(false)}
+                    backDropClick={() => setOpen(false)}
+                >
                     <Box sx={{ height: "80%" }}>Empty bar</Box>
                     <Divider />
                     <Box>
@@ -69,8 +80,141 @@ const Page = () => {
                             EN
                         </Box>
                     </Box>
+                </Sidebar>
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: 4,
+                        padding: 2,
+                        paddingLeft: mainContentWidth,
+                        transition: "all 0.25s ease-in-out",
+                    }}
+                >
+                    <Typography sx={{ fontWeight: "bold" }}>
+                        Frequently used apps
+                    </Typography>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            textWrap: "wrap",
+                            gap: 4,
+                            textAlign: "center",
+                        }}
+                    >
+                        <Box
+                            sx={{
+                                height: "6rem",
+                                width: "6rem",
+                                cursor: "pointer",
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    backgroundColor: "background.paper",
+                                    height: "6rem",
+                                    width: "6rem",
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    borderRadius: "1rem",
+                                }}
+                            >
+                                <ChatBubbleIcon fontSize="large" />
+                            </Box>
+                            <Typography
+                                sx={{ marginTop: 2, fontWeight: "bold" }}
+                            >
+                                Powerflow
+                            </Typography>
+                        </Box>
+                        <Box
+                            sx={{
+                                height: "6rem",
+                                width: "6rem",
+                                cursor: "pointer",
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    backgroundColor: "background.paper",
+                                    height: "6rem",
+                                    width: "6rem",
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    borderRadius: "1rem",
+                                }}
+                            >
+                                <RocketLaunchIcon fontSize="large" />
+                            </Box>
+                            <Typography
+                                sx={{
+                                    marginTop: 2,
+                                    fontWeight: "bold",
+                                }}
+                            >
+                                Accelerate System
+                            </Typography>
+                        </Box>
+                        <Box
+                            sx={{
+                                height: "6rem",
+                                width: "6rem",
+                                cursor: "pointer",
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    backgroundColor: "background.paper",
+                                    display: "flex",
+                                    height: "6rem",
+                                    width: "6rem",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    borderRadius: "1rem",
+                                }}
+                            >
+                                <BubbleChartIcon fontSize="large" />
+                            </Box>
+                            <Typography
+                                sx={{
+                                    marginTop: 2,
+                                    fontWeight: "bold",
+                                }}
+                            >
+                                Knowledge Graph
+                            </Typography>
+                        </Box>
+                        <Box
+                            sx={{
+                                height: "6rem",
+                                width: "6rem",
+                                cursor: "pointer",
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    backgroundColor: "background.paper",
+                                    height: "6rem",
+                                    width: "6rem",
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    borderRadius: "1rem",
+                                }}
+                            >
+                                <FolderIcon fontSize="large" />
+                            </Box>
+                            <Typography
+                                sx={{ marginTop: 2, fontWeight: "bold" }}
+                            >
+                                My files
+                            </Typography>
+                        </Box>
+                    </Box>
                 </Box>
-            </Sidebar>
+            </Box>
         </Box>
     );
 };
