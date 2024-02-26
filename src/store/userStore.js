3c92d85e306edba008f59c221517f56084ed8b0c
RichardPickman <drezzerock@gmail.com>
2024-02-26 18:44:01 +0100

    revisit store

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,3 +1,5 @@
+'use client';
+
 import axiosRequest from 'src/axios/axiosRequestWithoutBearer';
 import { getAccessToken } from 'src/axios/getAccessToken';
 import authConfig from 'src/configs/auth';
@@ -36,53 +38,52 @@ const prefetchSavedUser = async () => {
   }
 }
 
-const userStore = create((set) => ({
+export const userStore = create((set) => ({
   user: null,
-  loading: false,
-  setUser: (data) => set(() => ({ user: data })),
-  login: async (data) => {
-    set({ loading: true });
-
-    const response = await axiosRequest(authConfig.loginEndpoint, 'post', data);
-
-    set({ user: { ...data, role: 'client' } })
-
-    if (data.rememberMe) {
-      window.localStorage.setItem('userData', JSON.stringify(data))
-      window.localStorage.setItem(authConfig.storageTokenKeyName, response.data.accessToken)
-      window.localStorage.setItem('refreshToken', response.data.refreshToken)
-    }
-
-    set({ loading: false });
-  },
-  register: async (data) => {
-    set({ loading: true });
-
-    await axiosRequest(authConfig.registerEndpoint, 'post', data)
-
-    set({ loading: false });
-  },
-  logout: () => {
-    set(() => ({ user: null }))
-
-    window.localStorage.removeItem('userData')
-    window.localStorage.removeItem(authConfig.storageTokenKeyName)
-    window.localStorage.removeItem('refreshToken')
-  },
-  forgotMyPassword: async (data) => {
-    set({ loading: true });
-
-    const token = window.localStorage.getItem(authConfig.storageTokenKeyName);
-
-    await axiosRequest(authConfig.forgotMyPasswordEndpoint, 'post', { ...data, token });
-
-    set({ loading: false });
-  },
+  setUser: (data) => set(({ user: data })),
 }));
 
-export const useUserStore = () => userStore
+export const login = async (data) => {
+  const response = await axiosRequest(authConfig.loginEndpoint, 'post', data);
+
+  userStore.setState({ user: { ...data, role: 'client' } });
+
+  if (data.rememberMe) {
+    window.localStorage.setItem('userData', JSON.stringify(data))
+    window.localStorage.setItem(authConfig.storageTokenKeyName, response.data.accessToken)
+    window.localStorage.setItem('refreshToken', response.data.refreshToken)
+  }
+
+  return;
+}
+
+export const register = async (data) => {
+  await axiosRequest(authConfig.registerEndpoint, 'post', data)
+
+  return;
+}
+
+export const logout = () => {
+  userStore.setState({ user: null });
+
+  window.localStorage.removeItem('userData')
+  window.localStorage.removeItem(authConfig.storageTokenKeyName)
+  window.localStorage.removeItem('refreshToken')
+
+  return;
+}
+
+export const forgotMyPassword = async (data) => {
+  const token = window.localStorage.getItem(authConfig.storageTokenKeyName);
+
+  await axiosRequest(authConfig.forgotMyPasswordEndpoint, 'post', { ...data, token });
+
+  return;
+}
 
 prefetchSavedUser()
-  .then(user => {
-    userStore.setState(() => ({ user, loading: false }))
+  .then(data => {
+    const user = data ? { ...data, role: 'client' } : null;
+
+    userStore.setState(() => ({ user }))
   })
